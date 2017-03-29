b705663d7931ccd0459f81ac209e6000a17fcecd
Landon Sherwood <landonsherwood23@gmail.com>
2017-03-29 02:08:45 -0500

    chat working, need to implement scroll

--- src/app/components/app-header/app-header.component.js
+++ src/app/components/app-header/app-header.component.js
@@ -1,21 +1,76 @@
 class AppComponentController{
-    constructor(){
+    constructor($mdSidenav,$rootScope,$timeout){
         'ngInject';
+        this.$mdSidenav = $mdSidenav;
+        this.msgVal = '';
 
+        // Message Inbox
+        $rootScope.messages = [];
 
+        // Send Messages
+        this.send = function() {
+            var message = {
+                user: {
+                  name: 'Brandon Shaw'
+                },
+
+                data: this.msgVal,
+
+                icon: 'img/user.png',
+
+                type: 'sent'
+            };
+
+            $rootScope.messages.push(message);
+
+            this.msgVal = '';
+
+            $timeout(function(){
+              //Fake reply message
+              var fakeMessage = {
+                  user: {
+                    name: 'Jello'
+                  },
+
+                  data: 'Jello There',
+
+                  icon: 'img/jello-clipart-1.jpg',
+
+                  type: 'received'
+                };
+
+                $rootScope.messages.push(fakeMessage);
+            }, 5000);
+
+            $timeout(function(){
+              //Fake reply message
+              var fakeMessage = {
+                  user: {
+                    name: 'Duck'
+                  },
+
+                  data: "What's Quackin'",
+
+                  icon: 'img/duck.png',
+
+                  type: 'received'
+                };
+
+                $rootScope.messages.push(fakeMessage);
+            }, 3000);
+
+        };
     }
 
     $onInit(){
 
     }
 
-
-
-
+    openRightMenu() {
+      this.$mdSidenav('right').toggle();
+    };
 
 }
-
-
 
 export const AppHeaderComponent = {
     template: require('./app-header.component.html'),
